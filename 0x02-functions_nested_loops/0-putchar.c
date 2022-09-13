858babe8207a5d4bb69133ff48f285dfcc88cd7d
Adikaibe-Sandra <akinfoyekusandra@gmail.com>
2022-09-13 13:57:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - point
- * Description - 'prints _putchar, followed by a new line'
+ * main - Entry point
+ * Description: 'Prints _putchar, followed by a new line'
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "Pedantic";
+	char *sh = "main";
 
 	while (*sh)
 	{
