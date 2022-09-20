2c66ca27455d40ee9915cf47caaa4f02b616cedc
Adikaibe-Sandra <akinfoyekusandra@gmail.com>
2022-09-19 18:50:56 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,18 +7,17 @@
  */
 void puts_half(char *str)
 {
-	int i, n;
+	int len = 0, i = 0, st;
 
-	for (i = 0; str[i] != '\0'; ++i)
-		if (i % 2 == 0)
+	while (*(str + len) != '\0')
 	{
-		for (n = i / 2; str[n] != '\0'; ++n)
-			_putchar(str[n]);
+		len++;
 	}
-	else
+	st = ((len - 1) / 2) + 1;
+
+	for (i = st; i < len; i++)
 	{
-		for (n = ((i - 1) / 2) + 1; str[n] != '\0'; ++n)
-			_putchar(str[n]);
+		_putchar(*(str + i));
 	}
-	_putchr('\n');
+	_putchar('\n');
 }
