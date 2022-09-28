e3721fa52d9b6c942c2a2b2b5e5bf44de913b082
Adikaibe-Sandra <akinfoyekusandra@gmail.com>
2022-09-28 14:17:32 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		summ++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
