daca1e37d3aa31ac94092444196f2d4e574777e9
Adikaibe-Sandra <akinfoyekusandra@gmail.com>
2022-10-12 11:15:52 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,11 @@
  * print_name - prints the name
  * @name: name
  * @f: function
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
