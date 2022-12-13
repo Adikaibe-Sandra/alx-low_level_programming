07be59d1ada504e91e8b43ca59184a065bab6577
Adikaibe-Sandra <akinfoyekusandra@gmail.com>
2022-12-13 13:44:22 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, r = 0;
+	int i = 0, op = 0;
 
-	while (i)
+	while (op == 0)
 	{
-		if (sl[i] == '\0' && s2[i] == '\0')
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		else if (sl[i] == '\0')
-		{
-			r = s2[i];
-			break;
-		}
-		else if (sl[i] != s2[i])
-		{
-			r = sl[i] - s2[i];
-			break;
-		}
-		else
-			i++;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	return (r);
+
+	return (op);
 }
